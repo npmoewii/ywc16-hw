af7da02303056e0ab20c2026e36d4cc51c5283be
Natchaporn Ponkitsaran <natchaporn.pon@gmail.com>
2018-11-10 13:54:04 +0700

    Updates

---
+++ precache-manifest.b26458c65488c240af66273631f759d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e64cdae75163fab98a7ced6e0949a35d",
+    "url": "/ywc16-hw/static/media/16603.e64cdae7.jpg"
+  },
+  {
+    "revision": "922547bc99b34947dfab",
+    "url": "/ywc16-hw/static/js/runtime~main.922547bc.js"
+  },
+  {
+    "revision": "09df4044c2dab420673d",
+    "url": "/ywc16-hw/static/js/main.09df4044.chunk.js"
+  },
+  {
+    "revision": "2608194a74f0e5049a63",
+    "url": "/ywc16-hw/static/js/1.2608194a.chunk.js"
+  },
+  {
+    "revision": "09df4044c2dab420673d",
+    "url": "/ywc16-hw/static/css/main.1ee463ae.chunk.css"
+  },
+  {
+    "revision": "2608194a74f0e5049a63",
+    "url": "/ywc16-hw/static/css/1.a014c170.chunk.css"
+  },
+  {
+    "revision": "baf519537c3ac6e7b6ca5a3778f71cac",
+    "url": "/ywc16-hw/index.html"
+  }
+];
